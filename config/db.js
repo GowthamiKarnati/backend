71f03c2c76fed064ad2b9903176d289ecb53d97e
GowthamiKarnati <113590933+GowthamiKarnati@users.noreply.github.com>
2023-06-14 18:59:02 +0530

    modified mongo uri

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB= () =>{
-    mongoose.connect('mongodb+srv://user123:user123@cluster0.kj1mvwv.mongodb.net/blog',{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
+    mongoose.connect('mongodb+srv://user123:user123@cluster0.kj1mvwv.mongodb.net/blog',{})
 
 .then(db =>console.log('DB is connected'))
 .catch(err => console.log(err));
