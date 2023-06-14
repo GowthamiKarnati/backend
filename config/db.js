144ac516cfaa8a5bf7241641a4a62d1f7131b542
GowthamiKarnati <113590933+GowthamiKarnati@users.noreply.github.com>
2023-06-14 13:38:30 +0530

    c2

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const connectDB= () =>{
+    mongoose.connect('mongodb+srv://user123:user123@cluster0.kj1mvwv.mongodb.net/blog',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+
+.then(db =>console.log('DB is connected'))
+.catch(err => console.log(err));
+};
+module.exports = connectDB;
