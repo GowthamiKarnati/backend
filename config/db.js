10cda61411e6f8dfda8dba95b36cb964e2b46eee
GowthamiKarnati <113590933+GowthamiKarnati@users.noreply.github.com>
2023-06-14 19:02:09 +0530

    verbose mongose error

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectDB= () =>{
+const connectDB = () => {
     mongoose.connect('mongodb+srv://user123:user123@cluster0.kj1mvwv.mongodb.net/blog',{})
-
-.then(db =>console.log('DB is connected'))
-.catch(err => console.log(err));
+        .then(db =>console.log('DB is connected'))
+        .catch(err => console.error("MongoDB Connection Failed... :: " + err));
 };
 
 module.exports = connectDB;
