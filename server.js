c0650e12942922f1d7c4c20db32d7fdec2967638
GowthamiKarnati <113590933+GowthamiKarnati@users.noreply.github.com>
2023-06-14 20:13:10 +0530

    added mongodb op

--- server.js
+++ server.js
@@ -14,10 +14,34 @@ mongoose.connect('mongodb+srv://gowthami:gowthami-atlas@blog-cluster.zy7uvvt.mon
     .then(() => console.log('[server][MongoDB Atlas] Connected to Database'))
     .catch(err => console.error('[server][Mongoose Error] ' + err));
 
+const newSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const Test = mongoose.model('test', newSchema);
+
 app.get('/', (req, res) => {
     res.json({
         msg: "Hello World!"
     });
 });
+
+app.get('/test', (req, res) => {
+    const newData = new Test({
+        username: "Dhamareshwar",
+        password: "123456"
+    });
+
+    newData.save()
+        .then(data =>{
+            console.log('Data Saved');
+            res.json(data)
+        })
+        .catch(err => {
+            console.error('mongoose save error ' + err);
+            res.json('Something error')
+        })
+})
 
 app.listen(PORT, () => console.log(`[server] Server up and running @${PORT}`))
